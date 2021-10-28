7df4c37a01a5648e5a6b1ce63a35d2fa7c4054fa
Caleb Broderick <liquidarkness1997@gmail.com>
2021-10-28 16:23:49 -0600

    init testing, writing first test

--- tests.js
+++
@@ -1,1 +0,0 @@
-const functions = require('./linkedListAddTwoNumbers/linkedListAddTwoNumbers.js');
---
+++ tests.test.js
@@ -0,0 +1,7 @@
+const functions = require('./linkedListAddTwoNumbers/linkedListAddTwoNumbers.js');
+
+describe('Linked List Add Two Numbers', () => {
+  it('should be a function', () => {
+    expect(functions.addTwoNumbers).toBe(function);
+  })
+})
